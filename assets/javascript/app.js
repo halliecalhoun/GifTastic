e1efda56bb5d4ffeaa88af2994f33c5bff49d3a2
Hallie Calhoun <hallieacalhoun@gmail.com>
2019-06-04 02:02:32 -0700

    all basic js functionality built out. no styling

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,49 @@
 var topics = ["burger", "pizza", "sandwich", "salad", "soup", "chips", "steak", "smoothie", "fries", "burrito", "taco"];
 var API_key = "vuhLAezXLof5lt9HMtQdMox0ZRd207JU";
 
+
+function displayFoodInfo() {
+
+    // YOUR CODE GOES HERE!!! HINT: You will need to create a new div to hold the JSON.
+    var gifSearch = $(this).attr("data-name");
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + gifSearch + "&limit=10&apikey=" + API_key;
+    console.log(queryURL);
+
+    $.ajax({
+      url:queryURL,
+      method: "GET"
+    }).then(function (response) {
+        console.log(response);
+        $("#gif-div").empty();
+   
+        for (var i = 0; i < response.data.length; i++) {
+        var gifInfoDiv = $("<div>");
+        gifInfoDiv.addClass("gifInfoContent");
+
+        var foodRating = $("<h1>").text("Rating: " + response.data[i].rating);
+
+        var imageDiv = $("<img>");
+        imageDiv.attr("src", response.data[i].images["original_still"].url);
+        imageDiv.attr("data-still", response.data[i].images["original_still"].url);
+        imageDiv.attr("data-original", response.data[i].images["original"].url);
+        imageDiv.addClass("gif");
+        // var artistURL = $("<a>").attr("href", response.url).append(artistName);
+        // var artistImage = $("<img>").attr("src", response.thumb_url);
+        // var trackerCount = $("<h2>").text(response.tracker_count + " fans tracking this artist");
+        // var upcomingEvents = $("<h2>").text(response.upcoming_event_count + " upcoming events");
+        // var goToArtist = $("<a>").attr("href", response.url).text("See Tour Dates");
+        gifInfoDiv.append(foodRating);
+        gifInfoDiv.append(imageDiv);
+        $("#gif-div").append(gifInfoDiv);
+        
+  
+        }
+
+     
+    })
+  };
+
+
 function renderButtons() {
 
     // Deleting the buttons prior to adding new movies
@@ -15,6 +58,7 @@ function renderButtons() {
       var newButton = $("<button>");
       // Adding a class of movie to our button
       newButton.addClass("food");
+      newButton.addClass("btn btn-primary");
       // Adding a data-attribute
       newButton.attr("data-name", topics[i]);
       // Providing the initial button text
@@ -22,4 +66,23 @@ function renderButtons() {
       // Adding the button to the buttons-view div
       $("#buttons-view").append(newButton);
     }
-  }                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                  
+  }  
+  
+  // Event handler for user clicking the select-artist button
+  $("#add-gif").on("click", function(event) {
+    // Preventing the button from trying to submit the form
+    event.preventDefault();
+    // Storing the artist name
+    var getGif = $("#gif-input").val().toLowerCase().trim();
+    if (getGif != "") {
+        topics.push(getGif);
+
+    }
+    renderButtons();
+    
+   
+  });
+  $(document).on("click", ".food", displayFoodInfo);
+
+      // Calling the renderButtons function to display the intial buttons
+      renderButtons();
